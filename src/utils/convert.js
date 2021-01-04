741a58257bae25e1e4739ce3166998b249882dd1
Thomas Neal <nealthom@gmail.com>
2021-01-04 03:26:07 -0600

    cleaned up data

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,26 +1,30 @@
 import { date2ms } from "./date2ms";
 
 export default (array) => {
+  const MaxMachines = 7;
   const keys = array[0];
-  const converted = [];
+  let hours = [];
   for (let i = 1; i < array.length; i++) {
     let newObj = {};
     for (let j = 0; j < keys.length; j++) {
-      if (array[i][j] && j === 0) {
+      if (array[i][j] && j === 0 && i === 1) {
         // newObj[keys[j]] = array[i][j];
-
+        console.log(i);
         newObj[keys[j]] = date2ms(array[i][j]);
       } else if (
         keys[j] === "Hour" ||
         keys[j] === "TotalInPlay" ||
-        keys[j] === "Machines" ||
         keys[j] === "DOW"
       ) {
         newObj[keys[j]] = array[i][j];
       }
     }
-    converted.push(newObj);
+    hours.push(newObj);
   }
+  console.log(hours.shift());
+  hours.pop();
+  array.pop();
+  const max = array.pop()[MaxMachines];
 
-  return converted;
+  return { max, hours };
 };
