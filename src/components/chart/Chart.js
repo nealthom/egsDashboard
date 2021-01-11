44e4f6a0c94019fc084aa898f22b6bb40a407e5a
Thomas Neal <nealthom@gmail.com>
2021-01-11 06:51:42 -0600

    cleaning up code organization

---
+++ src/components/chart/Chart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+import LineChart from "./LineChart";
+
+const ChartContainer = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > h2,
+  h3 {
+    margin: 0;
+    padding: 0.25rem;
+  }
+`;
+
+export default function Chart({ data }) {
+  return (
+    <ChartContainer>
+      <h2>{data.dayOfWeek}</h2>
+      <h3>{`${data.date.month}/${data.date.day}/${data.date.year}`}</h3>
+      <LineChart input={data} />
+    </ChartContainer>
+  );
+}
