df4f0a39c93c1bd167ad360db41867f50244d7f4
Thomas Neal <nealthom@gmail.com>
2021-01-11 06:01:43 -0600

    changed app structure

--- src/App.js
+++ src/App.js
@@ -1,48 +1,6 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import MyDropzone from "MyDropzone";
-
-import LineChart from "components/LineChart";
-
-const Container = styled.div`
-  font-family: sans-serif;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ChartContainer = styled.div`
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  & > h2,
-  h3 {
-    margin: 0;
-    padding: 0.25rem;
-  }
-`;
+import React from "react";
+import Spreadsheet from "components/spreadsheet/Spreadsheet";
 
 export default function App() {
-  const [data, setData] = useState();
-
-  return (
-    <Container>
-      <h1>Spreadsheet</h1>
-      <MyDropzone set={setData} />
-      {data ? (
-        <ChartContainer>
-          <h2>{data.dayOfWeek}</h2>
-          <h3>{`${data.date.month}/${data.date.day}/${data.date.year}`}</h3>
-          <LineChart input={data} />
-        </ChartContainer>
-      ) : (
-        ""
-      )}
-    </Container>
-  );
+  return <Spreadsheet />;
 }
---
+++ src/components/spreadsheet/Spreadsheet.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import MyDropzone from "MyDropzone";
+
+import LineChart from "components/LineChart";
+
+const Container = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ChartContainer = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > h2,
+  h3 {
+    margin: 0;
+    padding: 0.25rem;
+  }
+`;
+
+
+
+export default function Spreadsheet() {
+  const [data, setData] = useState();
+
+  return (
+    <Container>
+      <h1>Spreadsheet</h1>
+      <MyDropzone set={setData} />
+      {data ? (
+        <ChartContainer>
+          <h2>{data.dayOfWeek}</h2>
+          <h3>{`${data.date.month}/${data.date.day}/${data.date.year}`}</h3>
+          <LineChart input={data} />
+        </ChartContainer>
+      ) : (
+        ""
+      )}
+    </Container>
+  );
+}
