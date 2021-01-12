d498040e49ac280707aa0fd61da12aec85d20fab
Thomas Neal <nealthom@gmail.com>
2021-01-12 05:03:07 -0600

    update conversion working on making making the spreadhsheet work with multiple days

--- src/utils/convertEGSOccupancy.js
+++ src/utils/convertEGSOccupancy.js
@@ -1,47 +1,27 @@
 import { date2ms } from "./date2ms";
 
 export default (array) => {
+  const wantedKeys = ["Business Date", "TotalInPlay", "DOW", "Hour"];
   const MaxMachines = 8;
   const keys = array.shift();
   let days = [];
   let hours = [];
-
+  let businessDate = null;
+  let day = null;
   let convertedArray = []; //convert each row into an object
   for (let i = 0; i < array.length; i++) {
     let tempObj = {};
     for (let j = 0; j < keys.length; j++) {
-      if (array[i][j] && keys[j]) tempObj[keys[j]] = array[i][j];
-      //tempObj.keys[j] = array[i][j];
+      if (array[i][j] && wantedKeys.indexOf(keys[j]) !== -1) {
+        tempObj[keys[j]] = array[i][j];
+      }
+    }
+    if ("Business Date" in tempObj) {
+      console.log(tempObj);
     }
     convertedArray.push(tempObj);
   }
-  console.log(JSON.stringify(convertedArray));
-  // for (let i = 1; i < array.length; i++) {
-  //   let newObj = {};
-  //   for (let j = 0; j < keys.length; j++) {
-  //     if (array[i][j] && j === 0 && i === 1) {
-  //       // newObj[keys[j]] = array[i][j];
-
-  //       newObj[keys[j]] = date2ms(array[i][j]);
-  //     } else if (
-  //       keys[j] === "Hour" ||
-  //       keys[j] === "TotalInPlay" ||
-  //       (keys[j] === "DOW" && array[i][j])
-  //     ) {
-  //       newObj[keys[j]] = array[i][j];
-  //     }
-  //   }
-  //   hours.push(newObj);
-  // }
-  // const tempDate = hours.shift();
-  // const date = tempDate["Business Date"];
-  // const maxPlayed = tempDate["TotalInPlay"];
-  // const dayOfWeek = tempDate.DOW;
-
-  // hours.pop();
-  // hours.pop();
-  // array.pop();
-  // const max = array.pop()[MaxMachines];
+  console.table(convertedArray);
 
   return days;
 };
